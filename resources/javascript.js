024b69896a835759c1412e48343e138b02dfcec6
Samuel Ondrek <samuel@ondrek.com>
2014-12-27 10:57:11 +0100

    Updated resources

---
+++ resources/javascript.js
@@ -0,0 +1,19 @@
+(function(){
+
+	"use strict";
+
+	$(document).ready(function(){
+
+		$('#backgroundCarousel').carousel({
+			interval: 6000,
+			keyboard : false
+		});
+
+		$('#partnersCarousel').carousel({
+			interval: 2000,
+			keyboard : false
+		});
+
+	});
+
+})();
